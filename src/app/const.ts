3327d21466b7066412f695cde6a7041c988de2d8
Rob R <rob_452@hotmail.co.uk>
2025-03-22 11:05:09 +0000

    fix localhost

--- src/app/const.ts
+++ src/app/const.ts
@@ -1,4 +1,5 @@
-// export const ROBRENDELLWEBSITE_URL = "https://robrendellwebsite.onrender.com";
-export const ROBRENDELLWEBSITE_URL = "http://localhost:7000";
+export const ROBRENDELLWEBSITE_URL = "https://robrendellwebsite.onrender.com";
 
 export const ROBS_FREE_DOG_WALKS = "robs-free-dog-walks";
+
+// export const ROBRENDELLWEBSITE_URL = "http://localhost:7000";
